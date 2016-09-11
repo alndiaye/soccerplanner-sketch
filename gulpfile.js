63baa9b0cb767130522c8cc3cc8922a4dabb0111
dobernard <d1bernard@yahoo.fr>
2016-09-11 21:20:29 +0200

    Mise en place de gulp et nunjucks

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('copy:js', function () {
+    return gulp.src('src/js/**/*.js',{base: 'src'})
+           .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:img', function () {
+    return gulp.src('src/img/**/*',{base: 'src'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:fonts', function () {
+    return gulp.src('src/fonts/**/*',{base: 'src'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:css', function () {
+    return gulp.src('src/css/**/*',{base: 'src'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:others', function () {
+    return gulp.src(['src/*.html','src/.htaccess','src/*.png','src/*.xml','src/*.ico','src/*.txt'],{base: 'src'})
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('templates', function () {
+    return gulp.src('src/templates/*.html')
+        .pipe(nunjucksRender({
+            path: ['src/templates/'] // String or Array
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('copy:statics', ['copy:js','copy:img','copy:fonts','copy:css','copy:others']);
+gulp.task('default', ['copy:statics', 'templates']);
